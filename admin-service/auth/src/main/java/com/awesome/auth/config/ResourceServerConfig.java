93eb5bccfd59004a40df82498bb08db90c2ce3a5
qingwenzhang <zhangpengcheng1987@126.com>
2020-01-18 22:40:13 +0800

    添加spring boot admin sever监控服务

--- admin-service/auth/src/main/java/com/awesome/auth/config/ResourceServerConfig.java
+++ admin-service/auth/src/main/java/com/awesome/auth/config/ResourceServerConfig.java
@@ -5,6 +5,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
+/**
+ * @author zhangpengcheng
+ */
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
@@ -12,9 +15,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
 
-        http.requestMatchers().antMatchers("/api/**", "/users/**", "/static/**")
+        http.requestMatchers().antMatchers("/api/**", "/users/**", "/static/**", "/actuator/**")
                 .and()
                 .authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
                 .antMatchers("/api/**", "/users/**").authenticated();
     }
 }
---
+++ admin-service/monitor/src/main/java/com/awesome/monitor/MonitorApplication.java
@@ -0,0 +1,20 @@
+package com.awesome.monitor;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author zhangpengcheng
+ */
+@SpringBootApplication
+@EnableAdminServer
+@EnableDiscoveryClient
+public class MonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class, args);
+    }
+
+}
