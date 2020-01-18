f3aa9be43dea9dfaf5dad64720f3297066e3c6c4
qingwenzhang <zhangpengcheng1987@126.com>
2020-01-18 20:11:25 +0800

    修改目录名

--- admin-service/config/src/main/java/com/awesome/config/SecurityConfig.java
+++ admin-service/config/src/main/java/com/awesome/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.awesome.demo.config;
+package com.awesome.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
